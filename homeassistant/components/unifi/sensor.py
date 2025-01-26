556a1efbf002f13efb94ac645cb8ccb37c380e75
Nerdix <70015952+N3rdix@users.noreply.github.com>
2025-01-26 08:18:22 +0000

    do not calculate, use user_num_sta

--- homeassistant/components/unifi/sensor.py
+++ homeassistant/components/unifi/sensor.py
@@ -123,22 +123,7 @@ def async_wlan_client_value_fn(hub: UnifiHub, wlan: Wlan) -> int:
 @callback
 def async_device_clients_value_fn(hub: UnifiHub, device: Device) -> int:
     """Calculate the amount of clients connected to a device."""
-
-    return len(
-        [
-            client.mac
-            for client in hub.api.clients.values()
-            if (
-                (
-                    client.access_point_mac != ""
-                    and client.access_point_mac == device.mac
-                )
-                or (client.access_point_mac == "" and client.switch_mac == device.mac)
-            )
-            and dt_util.utcnow() - dt_util.utc_from_timestamp(client.last_seen or 0)
-            < hub.config.option_detection_time
-        ]
-    )
+    return device.user_num_sta
 
 
 @callback
